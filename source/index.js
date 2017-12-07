05318946d49c09f479d31d3f83a36d8bf7b6d81b
mujichOk <alexeymalyarov@gmail.com>
2017-12-07 23:54:36 +0200

    initial commit

---
+++ source/index.js
@@ -0,0 +1,30 @@
+const readonlyKeys = {
+    'name': true,
+    'message': true
+};
+
+module.exports = ErrorFactory;
+
+
+function ErrorFactory(type, options = {}, BaseErrorType = Error) {
+    CustomError.prototype = Object.create(BaseErrorType.prototype);
+    return CustomError;
+
+
+    function CustomError(opts = {}) {
+        opts = Object.assign({}, options, opts, { name: type });
+        Object.assign(this, opts);
+
+        if (this.message) {
+            this.message = this.message.replace(/\$\{[^\}]*\}/g, (match) => {
+                const key = match.slice(2, -1);
+                const value = opts[key];
+                !readonlyKeys[key] && delete this[key];
+                return value;
+            });
+        }
+
+        BaseErrorType.call(this);
+        Error.captureStackTrace(this, CustomError);
+    }
+}
